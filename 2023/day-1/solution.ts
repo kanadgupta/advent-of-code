0da1fa8def59e9939f45dffe5dc23175f1271a3f
Kanad Gupta <git@kanad.dev>
2023-12-01 15:45:50 -0600

    refactor to return both sums

--- 2023/day-1/solution.ts
+++ 2023/day-1/solution.ts
@@ -9,7 +9,7 @@ const file = fs.readFileSync(
   { "encoding": "utf-8" },
 );
 
-const normalizedNumbers = {
+const part1 = {
   "1": "1",
   "2": "2",
   "3": "3",
@@ -19,44 +19,42 @@ const normalizedNumbers = {
   "7": "7",
   "8": "8",
   "9": "9",
-  // uncomment the following lines to get the second part of the answer
-  // "one": "1",
-  // "two": "2",
-  // "three": "3",
-  // "four": "4",
-  // "five": "5",
-  // "six": "6",
-  // "seven": "7",
-  // "eight": "8",
-  // "nine": "9",
 } as const;
 
-function getInt(match: keyof typeof normalizedNumbers) {
-  if (normalizedNumbers[match]) {
-    return normalizedNumbers[match];
-  } else {
-    throw new Error(`yikes, bad match: ${match}`);
-  }
-}
-
-const r = new RegExp(`${Object.keys(normalizedNumbers).join("|")}`, "g");
+const part2 = {
+  ...part1,
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
+} as const;
 
 const calibrations = file.trim().split("\n");
 
-let totalSum = 0;
+function getSum(part: typeof part1) {
+  let totalSum = 0;
+  const r = new RegExp(`${Object.keys(part).join("|")}`, "g");
 
-calibrations.forEach((cal) => {
-  const matches = Array.from(cal.matchAll(r));
-  if (matches === null) {
-    throw new Error(`no match found for ${cal}`);
-  } else {
-    const firstMatch = getInt(matches[0][0] as keyof typeof normalizedNumbers);
-    const lastMatch = getInt(
-      matches[matches.length - 1][0] as keyof typeof normalizedNumbers,
-    );
-    const currentNum = `${firstMatch}${lastMatch}`;
-    totalSum += Number(currentNum);
-  }
-});
+  calibrations.forEach((cal) => {
+    const matches = Array.from(cal.matchAll(r));
+    if (matches === null) {
+      throw new Error(`no match found for ${cal}`);
+    } else {
+      const firstMatch = part[matches[0][0] as keyof typeof part];
+      const lastMatch =
+        part[matches[matches.length - 1][0] as keyof typeof part];
+      const currentNum = `${firstMatch}${lastMatch}`;
+      totalSum += Number(currentNum);
+    }
+  });
 
-console.log("the total sum is", totalSum);
+  return totalSum;
+}
+
+console.log("the sum for part 1 is", getSum(part1));
+console.log("the sum for part 2 is", getSum(part2));
