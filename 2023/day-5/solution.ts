e92765e2de0288f6472d8e20caa15897cddaa89c
Kanad Gupta <git@kanad.dev>
2023-12-06 10:43:47 -0600

    refactor: consolidate, add comments

--- 2023/day-5/solution.ts
+++ 2023/day-5/solution.ts
@@ -31,6 +31,9 @@ function getInputs(fileName: string) {
 function getResult(input: string[]) {
   const allMappers = allMaps.map((map) => getCleanMap(map));
 
+  /**
+   * Generates a map (i.e., an array of number arrays) for a given map name (e.g., `seed-to-soil map:`)
+   */
   function getCleanMap(mapName: string) {
     const startingIndex = input.indexOf(mapName);
     if (startingIndex < 0) {
@@ -57,6 +60,11 @@ function getResult(input: string[]) {
     return arr;
   }
 
+  /**
+   * Gets the destination for a given source and map
+   * @param map an array of number arrays that is used for mapping
+   * (e.g., `[[88 18 7], [18 25 70]]`)
+   */
   function getDestination(source: number, map: [number, number, number][]) {
     let mapResult = source;
     const matchingLine = map.find((line) => {
@@ -72,10 +80,13 @@ function getResult(input: string[]) {
     return mapResult;
   }
 
+  /**
+   * Gets locations for an array of seeds
+   */
   function getLocations(
-    sources: number[],
+    seeds: number[],
   ): number[] {
-    let currentSources = sources;
+    let currentSources = seeds;
     allMappers.forEach((map) => {
       currentSources = currentSources.map((source) => {
         return getDestination(source, map);
@@ -84,22 +95,64 @@ function getResult(input: string[]) {
     return currentSources;
   }
 
+  /**
+   * Gets location for an seed
+   */
+  function getLocation(
+    seed: number,
+  ): number {
+    let currentSource = seed;
+    allMappers.forEach((map) => {
+      currentSource = getDestination(currentSource, map);
+    });
+    return currentSource;
+  }
+
   const dirtySeeds = input[0];
 
-  const seeds = dirtySeeds.split(": ")[1].split(" ").map(Number);
-  const locations = getLocations(seeds);
-  return Math.min(...locations);
+  const partOneSeeds = dirtySeeds.split(": ")[1].split(" ").map(Number);
+  const partOneLocations = getLocations(partOneSeeds);
+  const partOneResult = Math.min(...partOneLocations);
+
+  // part two
+  let partTwoResult = Infinity;
+  if (partOneSeeds.length % 2 !== 0) {
+    throw new Error("uneven number of seeds");
+  }
+
+  // construct the seed values from the pairs
+  for (let i = 0; i <= partOneSeeds.length / 2; i += 2) {
+    const seedStart = partOneSeeds[i];
+    const seedRange = partOneSeeds[i + 1];
+
+    for (let j = 0; j < seedRange; j++) {
+      // for each seed, determine its location and set that to the min
+      const currentSeed = seedStart + j;
+      const currentDestination = getLocation(currentSeed);
+      if (currentDestination < partTwoResult) {
+        partTwoResult = currentDestination;
+      }
+    }
+  }
+
+  return { partOneResult, partTwoResult };
 }
 
 const example = getInputs("example.txt");
 
-const expected = 35;
-const result = getResult(example);
-console.assert(expected === result, {
-  expected,
-  result,
+const partOneExpected = 35;
+const partTwoExpected = 46;
+const { partOneResult, partTwoResult } = getResult(example);
+console.assert(partOneExpected === partOneResult, {
+  partOneExpected,
+  partOneResult,
+});
+console.assert(partTwoExpected === partTwoResult, {
+  partTwoExpected,
+  partTwoResult,
 });
 
+// this took ~17 min to run on my MBP M1 Max
 const inputFile = "input.txt";
 console.log(
   `the results for ${inputFile} are`,
