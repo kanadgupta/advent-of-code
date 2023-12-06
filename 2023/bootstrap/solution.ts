deee3995b888ede7196d9cbe250a734208721197
Kanad Gupta <git@kanad.dev>
2023-12-06 11:48:38 -0600

    chore: add bootstrap

---
+++ 2023/bootstrap/solution.ts
@@ -0,0 +1,45 @@
+import fs from "node:fs";
+import path from "node:path";
+
+// deno/ESM patch, https://stackoverflow.com/a/61829368
+const __dirname = new URL(".", import.meta.url).pathname;
+
+/**
+ * Reads the file (must be in the same directory as this TS file)
+ * and returns its contents as an array, split by line
+ */
+function getInputs(fileName: string) {
+  const file = fs.readFileSync(
+    path.resolve(__dirname, fileName),
+    { "encoding": "utf-8" },
+  );
+
+  return file.trim().split("\n");
+}
+
+function getResult(input: string[]) {
+  let partOneResult = 0;
+  let partTwoResult = 0;
+
+  return { partOneResult, partTwoResult };
+}
+
+const example = getInputs("example.txt");
+
+const partOneExpected = Infinity;
+const partTwoExpected = Infinity;
+const { partOneResult, partTwoResult } = getResult(example);
+console.assert(partOneExpected === partOneResult, {
+  partOneExpected,
+  partOneResult,
+});
+console.assert(partTwoExpected === partTwoResult, {
+  partTwoExpected,
+  partTwoResult,
+});
+
+const inputFile = "input.txt";
+console.log(
+  `the results for ${inputFile} are`,
+  getResult(getInputs(inputFile)),
+);
